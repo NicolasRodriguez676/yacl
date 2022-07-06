6eb3f872a934348192e78bcb64ffe0d31ed175ba
NicolasRodriguez676 <nicolas.r.rodriguez676@gmail.com>
2022-07-05 19:41:45 -0500

    add parser walk types to yacl types

--- yacl/yacl_types.h
+++ yacl/yacl_types.h
@@ -1,7 +1,76 @@
 #ifndef _YACL_TYPES_H_
 #define _YACL_TYPES_H_
 
-#include "yP.h"
+#include "yacl.h"
+
+typedef enum Yacl_Actions {
+    ACTION_WRITE = 0,
+    ACTION_READ  = 1,
+    ACTION_PLOT  = 2,
+    ACTION_CONF  = 3,
+    ACTION_HELP  = 4,
+    ACTION_CLEAR = 5,
+
+    ACTION_NONE  = 0,
+    NUM_USR_DEF_ACTIONS  = 3
+
+} act_flag_e;
+
+typedef enum Yacl_Streams {
+    STREAM_NONE = 0,
+    STREAM_GPIO = 0,
+    STREAM_I2C  = 1,
+    STREAM_SPI  = 2,
+
+    NUM_STREAMS  = 3
+
+} str_flag_e;
+
+enum Option_Stack_Sizes {
+    OPT_DATA_SIZE   = 32,
+    OPT_REG_SIZE    = 32,
+    OPT_ADDR_SIZE   = 1,
+    OPT_STATE_SIZE  = 1,
+};
+
+enum Option_Stack_Offsets {
+    OPT_DATA_OFFSET   = 0,
+    OPT_REG_OFFSET    = OPT_DATA_SIZE,
+    OPT_ADDR_OFFSET   = OPT_DATA_SIZE + OPT_REG_SIZE,
+    OPT_STATE_OFFSET  = OPT_DATA_SIZE + OPT_REG_SIZE + OPT_STATE_SIZE,
+};
+
+enum Option_Stack_Indexes {
+    OPT_DATA      = 0,
+    OPT_REG       = 1,
+    OPT_ADDR      = 2,
+    OPT_STATE     = 3,
+
+    NUM_OPTIONS   = 4
+};
+
+typedef struct Option_Data_Stack {
+    char** args;
+    uint32_t* base;
+
+    uint32_t idx;
+
+} option_data_stack_s;
+
+typedef struct Walk_Stack {
+    char* bufr_begin;
+
+    // actions
+    act_flag_e action;
+
+    // streams
+    str_flag_e stream;
+
+    // options
+    bool valid_options[4];
+    option_data_stack_s options[4];
+
+} walk_stack_s;
 
 enum Control_Characters {
     DELIM_SPACE         = 32,
@@ -15,45 +84,6 @@ enum Buffer_Lengths {
     MAX_TOKEN_LEN    = 16,
     MAX_TOKENS       = 6,
     TOKENS_LEN_IDX   = 15
-};
-
-enum Action_Call_Back_Index {
-    READ_CB_IDX  = 0,
-    WRITE_CB_IDX = 1,
-    PLOT_CB_IDX = 2
-};
-
-enum Protocol_Call_Back_Indexes {
-    GPIO_CB_IDX     = 0,
-    I2C_CB_IDX      = 1,
-    SPI_CB_IDX      = 2,
-    HELP_CB_IDX     = 3,
-};
-
-enum Token_Arg_Indexes {
-    ARG_ADDR_IDX        = 2,
-    ARG_BEG_REG_IDX     = 3,
-    ARG_END_REG_IDX     = 4,
-    ARG_DATA0_IDX       = 4,
-    ARG_DATA1_IDX       = 5,
-
-    ARG_PROTOCOL_IDX    = 0,
-    ARG_ACTION_IDX      = 1
-};
-
-enum Token_Count_Checks {
-    ARG_READ_RANGE_CNT  = 4,
-    ARG_WRITE_RANGE_CNT = 5,
-
-    TOKEN_CNT_READ_MIN  = 4,
-    TOKEN_CNT_WRITE_MIN = 5,
-
-    TOKEN_CNT_PLOT_MIN  = 2
-};
-
-enum Lut_Array_Sizes {
-    NUM_PROTOCOLS = 4,
-    NUM_ACTIONS   = 3,
 };
 
 typedef struct error_desc {
@@ -78,14 +108,12 @@ typedef struct data_buffer {
 
 } data_buffer_t;
 
-typedef struct protocol_lut_cb {
-    char* protocols[NUM_PROTOCOLS + 1];
-    char* actions[NUM_ACTIONS];
-    cb_funcs_t funcs[(NUM_PROTOCOLS * NUM_ACTIONS) + 1][NUM_ACTIONS];
+typedef struct callback_lut {
+    cb_funcs_t funcs[NUM_USR_DEF_ACTIONS][NUM_STREAMS];
 
-    bool not_null_cbs[NUM_PROTOCOLS];
+    bool not_null_cbs[NUM_USR_DEF_ACTIONS][NUM_STREAMS];
     uint32_t num_not_null_cbs;
 
-} protocol_lut_cb_t;
+} cb_lut_t;
 
 #endif //_YACL_TYPES_H_
